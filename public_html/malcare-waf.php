d2494c051b5e0e7284c9775ae2b15270fd6ab9d9
juanpedro-gamesforlove <juanpedro@gamesforlove.org>
2025-05-07 18:57:29 -0600

    Jampack

---
+++ public_html/malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/1262701.cloudwaysapps.com/nfkwccxgbn/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/1262701.cloudwaysapps.com/nfkwccxgbn/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '90fbb581bc553f17eab07acf855cdaec');
+	include_once('/home/1262701.cloudwaysapps.com/nfkwccxgbn/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ public_html/wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'i0f2JSPmhUMEVgWiQEpqadYGYADY62BQpmsD76JTMGCWXqsqX7nqz7LvewCfgSsD');
+define('SECURE_AUTH_KEY',  'RwUfbobYj9vsHGcJPca2Ff3ReKW3bsG3NsIjH2c3FPR8s7hw7WpKWRsBV767gprV');
+define('LOGGED_IN_KEY',    '0jHKhNMyQYRCsRRGSgK7bQ5CLsugV8QEmGg3hEH5pXQ82auPCe3wmU6eJBTmDP15');
+define('NONCE_KEY',        '3sAcYrssqLQKzFratJs5t3ozNX0vahMhx2c55G1nMyXA93xiFDVdNG1U1jQeTu3f');
+define('AUTH_SALT',        'yIwYdHYEWnKm1hjmf1EVEf8Y8INHX5obFePQu6EYeYPopytV9LL6NPdSGuAggLT5');
+define('SECURE_AUTH_SALT', 'rPrMmu5EFhaUc7gedpqtU5azCKqh1LfDtn1dwMrrxIPwpJpVKUx23mCsxUgxuxsA');
+define('LOGGED_IN_SALT',   'j1JhiubRXGbdMp5XoNGK8vi9sY9uCvXB87yfXHmWHYbSifAyo8DfzjCdvKM0r46z');
+define('NONCE_SALT',       '4Tw1PxRW51F0UaTNDNwSpBXmu1RJz5aPM6BTijnYKcIwiquPR4xNyYjxhiaCcjtE');
